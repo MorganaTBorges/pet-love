a437c8814875be99fc2ddd6553a537ad13b6fbdd
Morgana Borges <morgana.t.borges@gmail.com>
2020-09-21 09:54:20 -0300

    botao adicionar animal

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,23 +1,28 @@
 class AnimalsController < ApplicationController
 
   def index
-      @animals = Animal.all
-    end
+    @animals = Animal.all
+  end
   
-    def new
-      @animal = Animal.new
-    end
-  
-    def create
-      @pessoa = Pessoa.find(params[:pessoa_id])
-      @animal = Animal.new(animal_params)
-      @pessoa.animal = @pessoa
-    end
-  
-  
-    private
-    def animal_params
-      params.require(:animal).permit(:name, :tipo, :custo_mensal)
-    end
+  def new
+    @animal = Animal.new
+    @pessoa = Pessoa.find(params[:pessoa_id])
+  end
+
+  def create
+    @pessoa = Pessoa.find(params[:pessoa_id])
+    @animal = Animal.new(animal_params)
+    @pessoa.animal = @pessoa
+  end
+
+
+  private
+  def animal_params
+    params.require(:animal).permit(:nome, :tipo, :custo_mensal)
+  end
+
+  def pessoa_params
+    params.require(:pessoa).permit(:nome, :documento, :data_de_nascimento)
+  end
 
 end
