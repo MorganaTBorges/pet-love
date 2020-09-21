92613c982e4574894aedc3b56a97964bfbe54b9b
Morgana Borges <morgana.t.borges@gmail.com>
2020-09-21 17:39:15 -0300

    custo mensal alterado para float

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_20_000749) do
+ActiveRecord::Schema.define(version: 2020_09_21_203602) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "animals", force: :cascade do |t|
     t.string "nome"
-    t.integer "custo_mensal"
+    t.float "custo_mensal"
     t.string "tipo"
     t.bigint "pessoa_id", null: false
     t.datetime "created_at", precision: 6, null: false
